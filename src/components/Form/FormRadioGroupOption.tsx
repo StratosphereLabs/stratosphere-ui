ab4cb5e6628d685bda7bd9523fd72b629e401922
SuperCheese21 <ethan.shields21@gmail.com>
2023-05-07 20:36:45 -0700

    Added FormRadioGroup component

---
+++ src/components/Form/FormRadioGroupOption.tsx
@@ -0,0 +1,24 @@
+import { RadioGroup } from '@headlessui/react';
+import { Fragment } from 'react';
+import { Button, ButtonProps } from 'react-daisyui';
+
+export interface FormRadioGroupOptionProps extends ButtonProps {
+  value: string;
+}
+
+export const FormRadioGroupOption = ({
+  children,
+  disabled,
+  value,
+  ...props
+}: FormRadioGroupOptionProps) => {
+  return (
+    <RadioGroup.Option as={Fragment} disabled={disabled} value={value}>
+      {({ checked, disabled: isDisabled }) => (
+        <Button active={checked} disabled={isDisabled} {...props}>
+          {children}
+        </Button>
+      )}
+    </RadioGroup.Option>
+  );
+};
---
+++ src/components/Form/__tests__/FormRadioGroup.spec.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react';
+import FormProvider from './FormProvider';
+import { FormRadioGroupOption } from '../FormRadioGroupOption';
+import { FormRadioGroup } from '../FormRadioGroup';
+
+describe('FormRadioGroup', () => {
+  const name = 'radio';
+  const options = ['Option 1', 'Option 2', 'Option 3'];
+
+  const defaultProps = {
+    name,
+    children: options.map(option => (
+      <FormRadioGroupOption key={option} value={option}>
+        {option}
+      </FormRadioGroupOption>
+    )),
+  };
+
+  it('renders the label when labelText is provided', () => {
+    const labelText = 'Test Label Text';
+    const { container } = render(
+      <FormProvider defaultValues={{ radio: null }}>
+        <FormRadioGroup {...defaultProps} labelText={labelText} />
+      </FormProvider>,
+    );
+    expect(screen.getByText(labelText)).toBeInTheDocument();
+    expect(container.querySelector('label')).toBeInTheDocument();
+  });
+
+  it('does not render the label when labelText is not provided', () => {
+    const { container } = render(
+      <FormProvider defaultValues={{ radio: null }}>
+        <FormRadioGroup {...defaultProps} />
+      </FormProvider>,
+    );
+    expect(container.querySelector('label')).not.toBeInTheDocument();
+  });
+
+  it('renders with the correct class name', () => {
+    const className = 'testClassName';
+    const { container } = render(
+      <FormProvider defaultValues={{ radio: null }}>
+        <FormRadioGroup {...defaultProps} className={className} />
+      </FormProvider>,
+    );
+    expect(container.firstChild).toHaveClass(className);
+  });
+
+  it('renders with the correct value when a default value is provided', () => {
+    const defaultValue = options[1];
+    const { getByText } = render(
+      <FormProvider defaultValues={{ radio: defaultValue }}>
+        <FormRadioGroup {...defaultProps} />
+      </FormProvider>,
+    );
+    expect(getByText(defaultValue)).toHaveClass('btn-active');
+  });
+});
