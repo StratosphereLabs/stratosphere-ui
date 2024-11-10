84d66287470e15c0c0dba5d32fdce7f89655a26c
SuperCheese21 <ethan.shields21@gmail.com>
2024-11-10 12:03:42 -0700

    Migrate storybook from v6 to v8

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,22 +1,21 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-docs',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-      },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  addons: ['@storybook/addon-actions', '@storybook/addon-docs', {
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false,
     },
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-    '@storybook/addon-postcss',
-    '@storybook/addon-storysource',
-    'storybook-addon-themes',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  }, '@storybook/addon-interactions', '@storybook/addon-links', '@storybook/addon-postcss', '@storybook/addon-storysource', 'storybook-addon-themes', '@chromatic-com/storybook'],
+
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -48,3 +48,4 @@ export const decorators = [
     </Decorator>
   ),
 ];
+export const tags = ['autodocs'];
